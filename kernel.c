10f769584eeafdbc2de25fd403954ad966108bcb
ravielze <ravielz20@gmail.com>
2021-02-21 22:28:36 +0700

    readString

--- kernel.c
+++ kernel.c
@@ -4,14 +4,15 @@ void readString(char *string);
 void clear(char *buffer, int length);
 
 int mod(int a, int b);
-int strlen (char * string);
+int strlen(char *string);
 
 int main()
 {
 
     makeInterrupt21();
     // int a = interrupt(0x16, 0, 0, 0, 0);
-    while (1);
+    while (1)
+        ;
 }
 
 void clear(char *buffer, int length)
@@ -51,22 +52,46 @@ void printString(char *string)
     }
     return;
 }
+
 void readString(char *string)
 {
+    int input = interrupt(0x16, 0, 0, 0, 0);
+    char AH = (char)(input >> 8);
+    char AL = (char)(input);
+    int length = 0;
+    while (!(AL == '\r' || AH == 0x48 || AH == 0x50))
+    {
+        if (AL == '\b' && length > 0)
+        {
+            printString("\b \b");
+            string[length--] = 0x00;
+        }
+        else
+        {
+            string[length++] = AL;
+        }
+        input = interrupt(0x16, 0, 0, 0, 0);
+        AH = (char)(input >> 8);
+        AL = (char)(input);
+    }
+    printString("\n\r");
+    string[length] = 0;
     return;
 }
 
-int mod(int a, int b) {
-    return (a - b * (a / b)); 
+int mod(int a, int b)
+{
+    return (a - b * (a / b));
 }
 
-int strlen (char * string) {
-    char * a = string;
+int strlen(char *string)
+{
+    char *a = string;
     int ans = 0;
-    while (*a != 0x0) {
-        a ++;
+    while (*a != 0x0)
+    {
+        a++;
         ans += 1;
     }
     return ans;
-
 }
