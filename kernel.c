59281d7ed31557c2ea0bfb8c946ca0f61af67778
ravielze <ravielz20@gmail.com>
2021-02-21 20:29:06 +0700

    Fungsi clear

--- kernel.c
+++ kernel.c
@@ -1,26 +1,38 @@
-void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void handleInterrupt21(int AX, int BX, int CX, int DX);
 void printString(char *string);
 void readString(char *string);
-void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+void clear(char *buffer, int length);
 
-int main() {
-  makeInterrupt21();
-  while (1);
+int main()
+{
+    makeInterrupt21();
+    while (1)
+        ;
 }
 
-void handleInterrupt21 (int AX, int BX, int CX, int DX){
-    char AL, AH;
-	AL = (char) (AX);
-	AH = (char) (AX >> 8);
-    switch (AL) {
-        case 0x0:
-            printString(BX);
-            break;
-        case 0x1:
-            readString(BX);
-            break;
-        default:
-            printString("Invalid interrupt");
+void clear(char *buffer, int length)
+{
+    int i;
+    for (i = 0; i < length; i++)
+    {
+        *(buffer + i) = 0x000000;
     }
 }
 
+void handleInterrupt21(int AX, int BX, int CX, int DX)
+{
+    char AL, AH;
+    AL = (char)(AX);
+    AH = (char)(AX >> 8);
+    switch (AL)
+    {
+    case 0x0:
+        printString(BX);
+        break;
+    case 0x1:
+        readString(BX);
+        break;
+    default:
+        printString("Invalid interrupt");
+    }
+}
