6856311c3efc4c076fc4e040452601feb4a6c06d
ravielze <ravielz20@gmail.com>
2021-02-21 21:06:21 +0700

    Clear buffer update

--- kernel.c
+++ kernel.c
@@ -7,7 +7,8 @@ int main()
 {
 
     makeInterrupt21();
-    while (1);
+    while (1)
+        ;
 }
 
 void clear(char *buffer, int length)
@@ -15,7 +16,7 @@ void clear(char *buffer, int length)
     int i;
     for (i = 0; i < length; i++)
     {
-        *(buffer + i) = 0x000000;
+        *(buffer + i) = 0x0000;
     }
 }
 
@@ -39,7 +40,7 @@ void handleInterrupt21(int AX, int BX, int CX, int DX)
 
 void printString(char *string)
 {
-    char* p = string;
+    char *p = string;
     while (*p != 0x0)
     {
         interrupt(0x10, 0x0e00 + *p, 0x000A, 0, 0);
@@ -47,6 +48,7 @@ void printString(char *string)
     }
     return;
 }
-void readString(char *string) {
+void readString(char *string)
+{
     return;
 }
