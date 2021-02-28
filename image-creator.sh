f17ecde0a523ab5c3d706939a679c09c6976e90b
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-02-28 21:11:29 +0700

    fix run.sh

--- image-creator.sh
+++ image-creator.sh
@@ -1,12 +1,12 @@
 dd if=/dev/zero of=system.img bs=512 count=2880
-nasm ./assembly/bootloader.asm -o ./bin/bootloader
-dd if=./bin/bootloader of=system.img bs=512 count=1 conv=notrunc
+nasm ./assembly/bootloader.asm -o ./bin/bootloader.img
+dd if=./bin/bootloader.img of=system.img bs=512 count=1 conv=notrunc
 
 # Uncomment to create new map, files, and sectors image
 # dd if=/dev/zero of=./bin/map.img bs=512 count=1
 # dd if=/dev/zero of=./bin/files.img bs=512 count=2
 # dd if=/dev/zero of=./bin/sectors.img bs=512 count=1
 
-dd if=./bin/map.img of=system.img bs=512 count=1 seek=256 conv=notrunc
-dd if=./bin/files.img of=system.img bs=512 count=2 seek=257 conv=notrunc
-dd if=./bin/sectors.img of=system.img bs=512 count=1 seek=259 conv=notrunc
+# dd if=./bin/map.img of=system.img bs=512 count=1 seek=256 conv=notrunc
+# dd if=./bin/files.img of=system.img bs=512 count=2 seek=257 conv=notrunc
+# dd if=./bin/sectors.img of=system.img bs=512 count=1 seek=259 conv=notrunc
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 ./image-creator.sh > /dev/null 2>&1
 
-# Modify here if there is new kernel file
+#Modify here if there is new kernel file
 KERNEL_FILE=("kernel" "math")
 for f in ${KERNEL_FILE[@]}; do
     bcc -ansi -c -o ./bin/kernel/$f.o ./src/kernel/$f.c
@@ -11,10 +11,10 @@ nasm -f as86 ./assembly/kernel.asm -o ./bin/kernel_asm.o
 
 files="./bin/kernel_asm.o"
 for f in ${KERNEL_FILE[@]}; do
-    files=" $files ./bin/kernel/$f.o"
+    files="./bin/kernel/$f.o $files"
 done
 
 ld86 -o ./bin/kernel.img -d $files
-dd if=./bin/kernel.img of=system.img bs=512 conv=notrunc seek=1
+dd if=./bin/kernel.img of=system.img bs=512 conv=notrunc seek=1 > /dev/null 2>&1
 
-bochs -f if2230.config
+bochs -f if2230.config <<< "c" > /dev/null 2>&1
