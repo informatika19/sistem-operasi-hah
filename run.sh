ea144c409483e9a4fc68eff4a511b47ecf0ae5b5
ReinaldoAntolis <reinaldoantolis@gmail.com>
2021-02-28 21:42:59 +0800

    Fix run shell

--- run.sh
+++ run.sh
@@ -3,18 +3,18 @@
 
 # Modify here if there is new kernel file
 KERNEL_FILE=("kernel" "math")
-for f in $(KERNEL_FILE[@]); do
+for f in ${KERNEL_FILE[@]}; do
     bcc -ansi -c -o ./bin/kernel/$f.o ./src/kernel/$f.c
 done
 
-nasm -f as86 kernel.asm -o kernel_asm.o
+nasm -f as86 ./assembly/kernel.asm -o ./bin/kernel_asm.o
 
-files="./bin/kernel/kernel_asm.o"
-for f in $(KERNEL_FILE[@]); do
+files="./bin/kernel_asm.o"
+for f in ${KERNEL_FILE[@]}; do
     files=" $files ./bin/kernel/$f.o"
 done
 
-ld86 -o ./bin/kernel -d $params
-dd if=./bin/kernel of=system.img bs=512 conv=notrunc seek=1
+ld86 -o ./bin/kernel.img -d $files
+dd if=./bin/kernel.img of=system.img bs=512 conv=notrunc seek=1
 
 bochs -f if2230.config
