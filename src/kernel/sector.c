c104fcaddbc8f7dc01504483e7ce23f29b0bf24b
ravielze <ravielz20@gmail.com>
2021-03-15 17:25:07 +0700

    add clearSector

--- src/kernel/sector.c
+++ src/kernel/sector.c
@@ -8,6 +8,17 @@ void writeSector(char *buffer, int sector)
     interrupt(0x13, 0x301, buffer, div(sector, 36) * 0x100 + mod(sector, 18) + 1, mod(div(sector, 18), 2) * 0x100);
 }
 
+void clearSector(int sector)
+{
+    char buffer[512];
+    char i;
+    for (i = 0; i < 512; i++)
+    {
+        buffer[i] = 0x00;
+    }
+    writeSector(buffer, sector);
+}
+
 int availableSector(char *buffer)
 {
     int i;
