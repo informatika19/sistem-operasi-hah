9323f96015b2b5f682d1bfabc2f2880d17ead0cd
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-04-13 00:09:40 +0700

    cat error

--- assembly/lib.asm
+++ assembly/lib.asm
@@ -5,6 +5,7 @@
 ;lib.asm contains assembly functions that you can use in the shell
 
 global _interrupt
+global _returnToSender
 
 ;int interrupt (int number, int AX, int BX, int CX, int DX)
 _interrupt:
@@ -25,5 +26,9 @@ _interrupt:
 intr:	int 0x00	;call the interrupt (00 will be changed above)
 
 	mov ah,0	;we only want AL returned
+	pop bp
+	ret
+_returnToSender:
+	mov sp,bp
 	pop bp
 	ret
--- src/ext/cat.c
+++ src/ext/cat.c
@@ -1,14 +1,23 @@
 int main()
 {
     char currentDirectory;
-    char filename[16];
-    char buffer[512 * 16];
+    char *filename[16];
+    char *buffer[512 * 16];
     char flag;
-
-    getParam(&currentDirectory, filename);
-
-    readFile(buffer, filename, &flag, currentDirectory);
-
-    printString(buffer);
+    printString("cat called\n");
+    // clear(buffer, 512 * 16);
+    // printString("CAT CALLED\n");
+    // getParam(&currentDirectory, filename);
+    // if (currentDirectory == 0xFF)
+    // {
+    //     printString("Benar");
+    // }
+    returnToSender();
     return 0;
+    // printString(&currentDirectory);
+    // readFile(buffer, filename, &flag, currentDirectory);
+    // printString(buffer);
+
+    // printString("buffer");
+    // returnToSender();
 }
